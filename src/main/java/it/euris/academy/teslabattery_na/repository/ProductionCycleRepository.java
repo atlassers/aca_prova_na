2df8167ae70ac77835baf7aaefe45468c6ef5b0e
narda89 <nardanarda89@gmail.com>
2021-09-29 16:27:10 +0200

    seventh exam commit

--- src/main/java/it/euris/academy/teslabattery_na/repository/ProductionCycleRepository.java
+++ src/main/java/it/euris/academy/teslabattery_na/repository/ProductionCycleRepository.java
@@ -1,15 +1,29 @@
 package it.euris.academy.teslabattery_na.repository;
 
 import it.euris.academy.teslabattery_na.data.model.ProductionCycle;
+import it.euris.academy.teslabattery_na.repository.projection.ICyclesCountDate;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 public interface ProductionCycleRepository extends JpaRepository<ProductionCycle,Long> {
   @Query(value =
-      "SELECT COUNT(pc.production_cycle_id) AS 'completed cycles', "
-        +"pc.end_date as 'end_date' "
+      "SELECT COUNT(pc.production_cycle_id) AS 'countCycles', "
+        +"pc.end_date as 'endDate' "
           + "FROM teslabattery_na.prodcycle pc\n"
             + "WHERE pc.production_cycle_status = 1", nativeQuery = true)
-  public int countCompletedProdCyclesEachMonth();
+  public List<ICyclesCountDate> countCompletedProdCyclesEachMonth();
 
+  @Query(value =
+      "SELECT COUNT(pc.production_cycle_id) AS 'countCycles', "
+          +"pc.end_date as 'endDate' "
+          + "FROM teslabattery_na.prodcycle pc\n"
+          + "WHERE pc.production_cycle_status = 2", nativeQuery = true)
+  public List<ICyclesCountDate> countFailedProdCyclesEachMonth();
+
+  @Query(value =
+      "SELECT COUNT(pc.production_cycle_id) AS 'countCycles', "
+          +"pc.end_date as 'endDate' "
+          + "FROM teslabattery_na.prodcycle pc\n", nativeQuery = true)
+  public List<ICyclesCountDate> countTotalProdCyclesEachMonth();
 }
---
+++ src/main/java/it/euris/academy/teslabattery_na/repository/projection/ICyclesCountDate.java
@@ -0,0 +1,8 @@
+package it.euris.academy.teslabattery_na.repository.projection;
+
+import java.util.Date;
+
+public interface ICyclesCountDate {
+  Date getCountCycles();
+  Integer getEndDate();
+}
