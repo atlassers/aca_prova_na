887537ea312fd9ab9263f0593210af9100b694b3
narda89 <nardanarda89@gmail.com>
2021-09-29 16:19:50 +0200

    sixth exam commit

--- src/main/java/it/euris/academy/teslabattery_na/repository/ProductionCycleRepository.java
+++ src/main/java/it/euris/academy/teslabattery_na/repository/ProductionCycleRepository.java
@@ -2,7 +2,14 @@ package it.euris.academy.teslabattery_na.repository;
 
 import it.euris.academy.teslabattery_na.data.model.ProductionCycle;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ProductionCycleRepository extends JpaRepository<ProductionCycle,Long> {
+  @Query(value =
+      "SELECT COUNT(pc.production_cycle_id) AS 'completed cycles', "
+        +"pc.end_date as 'end_date' "
+          + "FROM teslabattery_na.prodcycle pc\n"
+            + "WHERE pc.production_cycle_status = 1", nativeQuery = true)
+  public int countCompletedProdCyclesEachMonth();
 
 }
