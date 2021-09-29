f2ffdde8e77511857df188b1e402aa4e26def776
narda89 <nardanarda89@gmail.com>
2021-09-29 11:20:48 +0200

    first exam push

---
+++ src/main/java/it/euris/academy/teslabattery_na/TeslabatteryNaApplication.java
@@ -0,0 +1,13 @@
+package it.euris.academy.teslabattery_na;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeslabatteryNaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeslabatteryNaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/euris/academy/teslabattery_na/TeslabatteryNaApplicationTests.java
@@ -0,0 +1,13 @@
+package it.euris.academy.teslabattery_na;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeslabatteryNaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
